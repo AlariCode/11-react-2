8d04cc38462d2f19c755d7a5ff282c96626952c0
AlariCode <antonlarichev@gmail.com>
2023-08-14 19:28:35 +0300

    Создание роутов

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { MouseEvent, useState } from 'react';
 import Button from './components/Button/Button';
 import Input from './components/Input/Input';
+import { Route, Routes } from 'react-router-dom';
+import { Menu } from './pages/Menu/Menu';
+import { Cart } from './pages/Cart/Cart';
+import { Error } from './pages/Error/Error';
 
 function App() {
 	const [counter, setCounter] = useState<number>(0);
@@ -14,6 +18,14 @@ function App() {
 			<Button onClick={addCounter}>Кнопка</Button>
 			<Button appearence='big' onClick={addCounter}>Кнопка</Button>
 			<Input placeholder='Email' />
+			<div>
+				<a href='/'>Меню</a>
+				<a href='/cart'>Корзина</a>
+			</div>
+			<Routes>
+				<Route path='/' element={<Menu />} />
+				<Route path='/cart' element={<Cart />} />
+			</Routes>
 		</>
 	);
 }
---
+++ src/pages/Error/Error.tsx
@@ -0,0 +1,3 @@
+export function Error() {
+	return <>Error</>;
+}
