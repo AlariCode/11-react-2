b3d98c8bfe063c63987a9664dd46917985f1ce1b
AlariCode <antonlarichev@gmail.com>
2023-08-14 19:37:06 +0300

    createBrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import { MouseEvent, useState } from 'react';
 import Button from './components/Button/Button';
 import Input from './components/Input/Input';
-import { Route, Routes } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { Menu } from './pages/Menu/Menu';
 import { Cart } from './pages/Cart/Cart';
 import { Error } from './pages/Error/Error';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Menu />
+	},
+	{
+		path: '/cart',
+		element: <Cart />
+	},
+	{
+		path: '*',
+		element: <Error />
+	}
+]);
 
 function App() {
 	const [counter, setCounter] = useState<number>(0);
@@ -22,10 +37,7 @@ function App() {
 				<a href='/'>Меню</a>
 				<a href='/cart'>Корзина</a>
 			</div>
-			<Routes>
-				<Route path='/' element={<Menu />} />
-				<Route path='/cart' element={<Cart />} />
-			</Routes>
+			<RouterProvider router={router} />
 		</>
 	);
 }
