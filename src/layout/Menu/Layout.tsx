be5505e8cc2316d4622261ccf563476fc9c6d442
AlariCode <antonlarichev@gmail.com>
2023-08-15 13:19:34 +0300

    useLocation

--- src/layout/Menu/Layout.tsx
+++ src/layout/Menu/Layout.tsx
@@ -1,8 +1,16 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useLocation } from 'react-router-dom';
 import styles from './Layout.module.css';
 import Button from '../../components/Button/Button';
+import { useEffect } from 'react';
+import cn from 'classnames';
 
 export function Layout() {
+	const location = useLocation();
+
+	useEffect(() => {
+		console.log(location);
+	}, [location]);
+
 	return <div className={styles['layout']}>
 		<div className={styles['sidebar']}>
 			<div className={styles['user']}>
@@ -11,7 +19,9 @@ export function Layout() {
 				<div className={styles['email']}>alari@ya.ru</div>
 			</div>
 			<div className={styles['menu']}>
-				<Link to='/' className={styles['link']}>
+				<Link to='/' className={cn(styles['link'], {
+					[styles.active]: location.pathname === '/'
+				})}>
 					<img src="/menu-icon.svg" alt="Иконка меню" />
 					Меню</Link>
 				<Link to='/cart' className={styles['link']}>
