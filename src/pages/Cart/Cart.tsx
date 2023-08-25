8688f96cae3f1d8c3f4167d8ec3b382ccf6450d1
AlariCode <antonlarichev@gmail.com>
2023-08-25 13:03:22 +0300

    Расчёт суммы

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -8,9 +8,19 @@ import axios from 'axios';
 import { PREFIX } from '../../helpers/API';
 import styles from './Cart.module.css';
 
+const DELIVERY_FEE = 169;
+
 export function Cart() {
 	const [cartProducts, setCardProducts] = useState<Product[]>([]);
 	const items = useSelector((s: RootState) => s.cart.items);
+	const total = items.map(i => {
+		const product = cartProducts.find(p => p.id === i.id);
+		if (!product) {
+			return 0;
+		}
+		return i.count * product.price;
+	}).reduce((acc, i) => acc += i, 0);
+
 
 	const getItem = async (id: number) => {
 		const { data } = await axios.get<Product>(`${PREFIX}/products/${id}`);
@@ -35,5 +45,19 @@ export function Cart() {
 			}
 			return <CartItem key={product.id} count={i.count} {...product} />;
 		})}
+		<div className={styles['line']}>
+			<div className={styles['text']}>Итог</div>
+			<div className={styles['price']}>{total}&nbsp;<span>₽</span></div>
+		</div>
+		<hr className={styles['hr']} />
+		<div className={styles['line']}>
+			<div className={styles['text']}>Доставка</div>
+			<div className={styles['price']}>{DELIVERY_FEE}&nbsp;<span>₽</span></div>
+		</div>
+		<hr className={styles['hr']} />
+		<div className={styles['line']}>
+			<div className={styles['text']}>Итог {items.length}</div>
+			<div className={styles['price']}>{total + DELIVERY_FEE}&nbsp;<span>₽</span></div>
+		</div>
 	</>;
 }
